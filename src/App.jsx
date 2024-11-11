71c14d64c7098a03d6ca2c81d6fa7e6c62c176de
CodeCrafted <codecrafteddesings@gmail.com>
2024-11-11 17:35:37 -0400

    Add a new form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Create from "./components/create";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Read from "./components/read";
 import Update from "./components/update";
+import home from "./components/home";
 
 const App = () => {
   return (
@@ -12,7 +13,7 @@ const App = () => {
             <Route path="/" element={<Create />} />
             <Route path="/read" element={<Read />} />
             <Route path="/update" element={<Update />} />
-          </Routes>
+            <Route path="home" element={<home />} />          </Routes>
         </BrowserRouter>
       </div>
     </>
---
+++ src/components/home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function home ()  {
+    return(
+        <div>Hello</div>
+    )
+}
+export default home;
